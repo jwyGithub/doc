67a4547661cd1fb3feb209d88843e2c0de26f54a
jiangwy <jiangwy@staryea.com>
2026-01-27 14:15:25 +0800

    fix: async

--- src/lib/cloudflare.ts
+++ src/lib/cloudflare.ts
@@ -1,11 +1,12 @@
-import { getCloudflareContext } from "@opennextjs/cloudflare";
+import { getCloudflareContext } from '@opennextjs/cloudflare';
 
 export async function getCloudflareEnv() {
-	const { env } = await getCloudflareContext();
-	return env as CloudflareEnv;
+    const { env } = await getCloudflareContext({ async: true });
+    return env as CloudflareEnv;
 }
 
 export async function getD1Database() {
-	const env = await getCloudflareEnv();
-	return env.DB;
+    const env = await getCloudflareEnv();
+    return env.DB;
 }
+
